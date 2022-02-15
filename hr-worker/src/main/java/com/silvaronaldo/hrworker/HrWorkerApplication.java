592504347d4f0fd055e7c941ee948aea0afe5623
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-15 14:54:44 -0300

    teste

---
+++ hr-worker/src/main/java/com/silvaronaldo/hrworker/HrWorkerApplication.java
@@ -0,0 +1,15 @@
+package com.silvaronaldo.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+			
+		//testes
+	}
+
+}
