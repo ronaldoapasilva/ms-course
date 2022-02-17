ac3740a8da4b99bcae4e5a1268e020149057872c
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-17 16:39:05 -0300

    hystrix fault tolerance

--- hr-payroll/src/main/java/com/silvaronaldo/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/silvaronaldo/hrpayroll/HrPayrollApplication.java
@@ -2,14 +2,15 @@ package com.silvaronaldo.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //comunicação de um projeto com outro @EnableFeignClients
 
 
 //Eureka já tem o balanceamento de carga então não precisa mais de riboon, balanceamento de cargar @RibbonClient(name = "hr-worker")
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
--- hr-payroll/src/main/java/com/silvaronaldo/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/silvaronaldo/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.silvaronaldo.hrpayroll.entities.Payment;
 import com.silvaronaldo.hrpayroll.services.PaymentService;
 
@@ -18,9 +20,16 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workedId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workedId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workedId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	//ystrix caminho alternativo para qdo o serviço não estiver funcionando 
+	public ResponseEntity<Payment> getPaymentAlternative(Long workedId, @PathVariable Integer days){
+		Payment payment = new Payment("Brann", 400.0, days);
 		return ResponseEntity.ok(payment);
 	}
 }
