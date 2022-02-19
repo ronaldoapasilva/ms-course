77ef9cd55ac201e25036b7267dc078b0702d5f78
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-19 15:55:44 -0300

    Project hr-oauth created

---
+++ hr-aouth/src/main/java/com/silvaronaldo/hroauth/HrAouthApplication.java
@@ -0,0 +1,13 @@
+package com.silvaronaldo.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAouthApplication.class, args);
+	}
+
+}
