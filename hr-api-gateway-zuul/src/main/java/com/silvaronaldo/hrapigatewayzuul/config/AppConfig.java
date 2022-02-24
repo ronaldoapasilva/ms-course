89585fc1086d9532b93dc33c415add187c3fde84
Ronaldo Ap. Silva <sinuca@gmail.com>
2022-02-24 17:14:03 -0300

    hr-api-gateway-zuul container

--- hr-api-gateway-zuul/src/main/java/com/silvaronaldo/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/silvaronaldo/hrapigatewayzuul/config/AppConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class AppConfig {
 	
 	
-	@Value("${jwt.secret}")
+	@Value("${jwt.secret:defaultValue}")
 	private String jwtSecret;
 	
 	
